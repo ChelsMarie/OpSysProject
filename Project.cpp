ad3d0a66aebad30eba077c0f3c8eafb6021127ec
ChelsMarie <vchelseamariev@gmail.com>
2019-07-09 21:39:35 -0400

    Update Project.cpp
    Updated error checking

--- Project.cpp
+++ Project.cpp
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <typeinfo>
 
-float interarrivalTime (float min, float max, float sum, int iter) {
+
+double interarrivalTime (double min, double max, double sum, int iter) {
 
   srand48( time( NULL ) );
 
@@ -13,8 +16,8 @@ float interarrivalTime (float min, float max, float sum, int iter) {
   {
       /* average should be 1/lambda ==> 1000 */
 
-    float r = drand48();   /* uniform dist [0.00,1.00) -- also check out random() */
-    float x = -log( r ) / lambda;  /* log() is natural log */
+    double r = drand48();   /* uniform dist [0.00,1.00) -- also check out random() */
+    double x = -log( r ) / lambda;  /* log() is natural log */
 
     /* avoid values that are far down the "long tail" of the distribution */
     if ( x > 3000 ) { i--; continue; }
@@ -25,28 +28,85 @@ float interarrivalTime (float min, float max, float sum, int iter) {
     if ( i == 0 || x > max ) { max = x; }
   }
 
-  float avg = sum / iterations;
+  double avg = sum / iterations;
 
 }
 
 int main(int argc, char* argv[]) {
 
 
-	//ERROR CHECING
-	if (argc != 9) {
-		std::cerr << "ERROR: You need 9 arguments." << std::endl;
-	}
+  //ERROR CHECING
+  if (argc != 9) {
+    std::cerr << "ERROR: You need 9 arguments." << std::endl;
+  }
 
 
- 	int seed; //argv[1] 
- 	float lambda; //argv[2] 
- 	float max;     //argv[3]     
-  int numProcesses; //argv[4]
-  	int tCS; //argv[5]
-  	double alpha; //argv[6]
-  	float min;    
-    float sum;    
-  	int iterations; 
+  int seed; //argv[1] 
+  double lambda; //argv[2] 
+  double max;     //argv[3]     
+    int numProcesses; //argv[4]
+    int tCS; //argv[5]
+    double alpha; //argv[6]
+    double timeSlice; //argv[7]
+    std::string rrAdd = "END"; //argv[8]
+    double min;    
+    double sum;    
+    int iterations; 
 
-  
+    if (typeid(argv[1]).name() == "int") {
+    seed = atoi(argv[1]);
+  }
+  else {
+    std::cerr << "ERROR: ARGV[1] is not an int" << std::endl;
+  }
+
+  if (typeid(argv[2]).name() == "double") {
+    lambda = atof(argv[2]);
+  }
+  else {
+    std::cerr << "ERROR: ARGV[2] is not a double" << std::endl;   
+  }
+
+  if (typeid(argv[3]).name() == "double") {
+    max = atof(argv[3]);
+  }
+  else {
+    std::cerr << "ERROR: ARGV[3] is not a double" << std::endl;   
+  }
+
+  if (typeid(argv[4]).name() == "int") {
+    numProcesses = atoi(argv[4])
+  }
+  else {
+    std::cerr << "ERROR: ARGV[4] is not an int" << std::endl;       
+  }
+
+  if (typeid(argv[5]).name() == "int") {
+    numProcesses = atoi(argv[5])
+  }
+  else {
+    std::cerr << "ERROR: ARGV[5] is not an int" << std::endl;       
+  }
+
+  if (typeid(argv[6]).name() == "int") {
+    numProcesses = atoi(argv[6])
+  }
+  else {
+    std::cerr << "ERROR: ARGV[6] is not a double" << std::endl;       
+  }
+
+  if (typeid(argv[7]).name() == "double") {
+    numProcesses = atof(argv[7])
+  }
+  else {
+    std::cerr << "ERROR: ARGV[7] is not a double" << std::endl;       
+  }
+
+  if (typeid(argv[8]).name() == "string") {
+    numProcesses = atoi(argv[8])
+  }
+  else {
+    std::cerr << "ERROR: ARGV[8] is not a string" << std::endl;       
+  }
+  return EXIT_SUCCESS;
 }
