d896b36782c92daf5a300b4e187cdd3d106f9d67
GeordieTate <tateg@rpi.edu>
2019-07-10 21:40:18 -0400

    Fixed header file and cpp file

--- process.cpp
+++ process.cpp
@@ -1,37 +1,21 @@
 #include "process.h"
 
-class process {
-
-	private:
-	
-	char letter;
-	int arrTime;
-	int numBursts;
-	int cpuTime;
-	int ioTime;
-
-	public:
-	char getLet();
-	int getArr();
-	int getBursts();
-	int getCPUTime();
-	int getIOTime();
-	
-
-};
-
 char process::getLet() {
 	return(this -> letter);
 }
-	int process::getArr() {
+
+int process::getArr() {
 	return(this -> arrTime);
 }
+
 int process::getBursts() {
 	return(this -> numBursts);
 }
-	int process::getCPUTime() {
+
+int process::getCPUTime() {
 	return(this -> cpuTime);
 }
-	int process::getIOTime() {
+
+int process::getIOTime() {
 	return(this -> ioTime);
 }
--- process.h
+++ process.h
@@ -1,1 +1,22 @@
-class process;
+#ifndef PROCESS_H
+#define PROCESS_H
+
+class process {
+	private:
+        char letter;
+        int arrTime;
+        int numBursts;
+        int cpuTime;
+        int ioTime;
+
+
+
+	public:
+	char getLet();
+	int getArr();
+	int getBursts();
+	int getCPUTime();
+	int getIOTime();
+};
+
+#endif
